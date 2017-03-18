2dd2ecca328747cff66327e95447a639fc3e90e1
userb <userb@kimo.info>
2017-03-18 21:57:42 +0900

    new rails project

---
+++ vendor/bundle/ruby/2.2.0/bin/sass-convert.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"c:\Programs\usbrumix2\ruby\2.2\x64-mingw32\bin\ruby.exe" "D:/Documents/Projects/Kimopedia/vendor/bundle/ruby/2.2.0/bin/sass-convert" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"c:\Programs\usbrumix2\ruby\2.2\x64-mingw32\bin\ruby.exe" "%~dpn0" %*
